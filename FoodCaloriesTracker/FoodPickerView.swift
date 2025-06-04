0003796b10a85b67fd6e897d3f5833cb1abe6450
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-04 21:06:39 +0300

    journal: add FoodPickerView

---
+++ FoodCaloriesTracker/FoodPickerView.swift
@@ -0,0 +1,67 @@
+//
+//  FoodPickerView.swift
+//  FoodCaloriesTracker
+//
+//  Created by Sylvan  on 04/06/2025.
+//
+
+import SwiftUI
+
+struct FoodPickerView: View {
+    @Environment(\.dismiss) var dismiss
+
+    let onFoodItemSelected: (FoodItem) -> Void
+
+    private let foodItems = FoodItem.foodItems.sorted { $0.name < $1.name }
+    private static var energyFormatter: EnergyFormatter = {
+        let formatter = EnergyFormatter()
+        formatter.unitStyle = .long
+        formatter.isForFoodEnergyUse = true
+        formatter.numberFormatter.maximumFractionDigits = 2
+        return formatter
+    }()
+
+    var body: some View {
+        NavigationStack {
+            List(foodItems) { item in
+                Button {
+                    onFoodItemSelected(item)
+                    dismiss()
+                } label: {
+                    HStack {
+                        Text(item.name)
+
+                        Spacer()
+
+                        Text(Self.energyFormatter.string(fromJoules: item.joules))
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                    }
+                    .foregroundStyle(.primary)
+                }
+                .buttonStyle(.plain)
+            }
+            .navigationTitle("Pick Food")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel", role: .cancel) {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    FoodPickerView { foodItem in
+        print("Selected: \(foodItem.name)")
+    }
+}
+
+#Preview("Dark Mode") {
+    FoodPickerView { foodItem in
+        print("Selected: \(foodItem.name)")
+    }
+    .preferredColorScheme(.dark)
+}
