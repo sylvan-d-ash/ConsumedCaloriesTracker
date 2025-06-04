696f12ac510458ad078be5b6a0f5724c3531ff47
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-04 16:04:50 +0300

    On view appear, request permission from user to access Health data

--- FoodCaloriesTracker/ProfileView-ViewModel.swift
+++ FoodCaloriesTracker/ProfileView-ViewModel.swift
@@ -12,6 +12,7 @@ enum ProfileItemType: String, CaseIterable, Identifiable {
     case age = "Age"
     case height = "Height"
     case weight = "Weight"
+    case sex = "Biological Sex"
 
     var id: String { rawValue }
 
@@ -20,12 +21,13 @@ enum ProfileItemType: String, CaseIterable, Identifiable {
         case .age: return NSLocalizedString("Age (yrs)", comment: "")
         case .height: return NSLocalizedString("Height", comment: "")
         case .weight: return NSLocalizedString("Weight", comment: "")
+        case .sex: return NSLocalizedString("Biological Sex", comment: "")
         }
     }
 
     var isEditable: Bool {
         switch self {
-        case .age: return false
+        case .age, .sex: return false
         default: return true
         }
     }
@@ -48,6 +50,7 @@ struct ProfileItemData: Identifiable {
         ProfileItemData(type: .age, value: "19"),
         ProfileItemData(type: .height, value: "160"),
         ProfileItemData(type: .weight, value: "60"),
+        ProfileItemData(type: .sex, value: "Female"),
     ]
 }
 
@@ -57,12 +60,48 @@ extension ProfileView {
         @Published var authorizationError: String?
         @Published var dataInteractionError: String?
 
+        private let healthKitManager = HealthKitManager()
+        private var cancellables = Set<AnyCancellable>()
+        private let itemsOrder: [ProfileItemType] = [.age, .sex, .height, .weight]
+
         init() {
             setupBindings()
         }
 
-        func setupBindings() {
+        private func setupBindings() {
             profileItems = ProfileItemData.example
+
+            healthKitManager.$profileData
+                .receive(on: DispatchQueue.main)
+                .map { [weak self] dictionaryData -> [ProfileItemData] in
+                    guard let `self` = self else { return [] }
+
+                    return self.itemsOrder.compactMap { itemType in
+                        dictionaryData[itemType]
+                    }
+                }
+                .assign(to: &$profileItems)
+
+            healthKitManager.$authorizationError
+                .receive(on: DispatchQueue.main)
+                .map { errorMessage -> String? in
+                    guard let errorMessage else { return nil }
+                    return "Authorization Error: \(errorMessage)"
+                }
+                .assign(to: &$authorizationError)
+
+            // Subscribe to dataFetchError from HealthKitManager
+            healthKitManager.$dataFetchError
+                .receive(on: DispatchQueue.main)
+                .map { errorMessage -> String? in
+                    guard let errorMessage, !errorMessage.isEmpty else { return nil }
+                    return "Data Error: \(errorMessage)"
+                }
+                .assign(to: &$dataInteractionError)
+        }
+
+        func onViewAppear() {
+            healthKitManager.requestAuthorizationAndLoadData()
         }
     }
 }
