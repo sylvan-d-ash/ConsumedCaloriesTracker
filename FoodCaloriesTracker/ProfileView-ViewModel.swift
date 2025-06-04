16041d9ead0e54f6b5dc078ed539b9707d90353c
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-04 13:48:34 +0300

    Add profile data model + view model

---
+++ FoodCaloriesTracker/ProfileView-ViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileView-ViewModel.swift
+//  FoodCaloriesTracker
+//
+//  Created by Sylvan  on 04/06/2025.
+//
+
+import SwiftUI
+
+enum ProfileItemType: String, CaseIterable, Identifiable {
+    case age = "Age"
+    case height = "Height"
+    case weight = "Weight"
+
+    var id: String { rawValue }
+
+    var displayName: String {
+        switch self {
+        case .age: return NSLocalizedString("Age (yrs)", comment: "")
+        case .height: return NSLocalizedString("Height", comment: "")
+        case .weight: return NSLocalizedString("Weight", comment: "")
+        }
+    }
+
+    var isEditable: Bool {
+        switch self {
+        case .age: return false
+        default: return true
+        }
+    }
+}
+
+struct ProfileItemData: Identifiable {
+    let type: ProfileItemType
+    var unitLabel: String
+    var value: String
+
+    var id: String { type.id }
+
+    init(type: ProfileItemType, unitLabel: String? = nil, value: String = NSLocalizedString("Not available", comment: "")) {
+        self.type = type
+        self.unitLabel = unitLabel ?? type.displayName
+        self.value = value
+    }
+}
+
+extension ProfileView {
+    final class ViewModel: ObservableObject {
+        @Published var profileItems = [ProfileItemData]()
+        @Published var authorizationError: String?
+        @Published var dataInteractionError: String?
+    }
+}
