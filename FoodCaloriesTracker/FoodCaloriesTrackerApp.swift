f8e327411a819808a43dd3c9bd50ac3f7a3ab8de
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-04 12:59:43 +0300

    Create project

---
+++ FoodCaloriesTracker/FoodCaloriesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodCaloriesTrackerApp.swift
+//  FoodCaloriesTracker
+//
+//  Created by Sylvan  on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodCaloriesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
