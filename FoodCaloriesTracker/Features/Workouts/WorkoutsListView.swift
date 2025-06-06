d5b0d1ab1cf333d3055e3e169252deb2d628020a
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-06 13:15:46 +0300

    workouts: add list view

---
+++ FoodCaloriesTracker/Features/Workouts/WorkoutsListView.swift
@@ -0,0 +1,21 @@
+//
+//  WorkoutsListView.swift
+//  FoodCaloriesTracker
+//
+//  Created by Sylvan  on 06/06/2025.
+//
+
+import SwiftUI
+
+struct WorkoutsListView: View {
+    init(healthKitManager: HealthKitManager) {
+        //
+    }
+
+    var body: some View {
+        NavigationStack {
+            Text("Hello universe")
+                .navigationTitle("Workouts")
+        }
+    }
+}
