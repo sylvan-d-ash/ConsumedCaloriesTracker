c9e02b431e3cef555b3896568f8d7d12758a9c75
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-07 20:56:47 +0300

    log-workout: implement design for LogWorkoutView

--- FoodCaloriesTracker/Features/Workouts/LogWorkoutView.swift
+++ FoodCaloriesTracker/Features/Workouts/LogWorkoutView.swift
@@ -9,7 +9,112 @@ import SwiftUI
 import HealthKit
 
 struct LogWorkoutView: View {
-    var body: some View {
-        Text("Hello universe!")
+    @Environment(\.dismiss) var dismiss
+    @StateObject private var viewModel: ViewModel
+
+    init(healthKitManager: HealthKitManager) {
+        _viewModel = .init(wrappedValue: .init(healthKitManager: healthKitManager))
     }
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Workout Details") {
+                    Picker("Activity Type", selection: $viewModel.selectedActivityType) {
+                        ForEach(viewModel.availableTypes) { type in
+                            Text(type.displayName)
+                                .tag(type)
+                        }
+                    }
+
+                    DatePicker("Start Time", selection: $viewModel.startDate)
+
+                    HStack {
+                        Text("Duration (minutes)")
+
+                        Spacer()
+
+                        TextField("Minutes", value: $viewModel.durationMinutes, formatter: formatter)
+                            .keyboardType(.decimalPad)
+                            .multilineTextAlignment(.trailing)
+                            .frame(width: 80)
+                    }
+
+                    HStack {
+                        Text("Active Energy (kcal)")
+
+                        Spacer()
+
+                        TextField("kcal", text: $viewModel.activeEnergy)
+                            .keyboardType(.decimalPad)
+                            .multilineTextAlignment(.trailing)
+                            .frame(width: 100)
+                    }
+
+                    if viewModel.showDistanceField {
+                        VStack(alignment: .leading) {
+                            Text("Distance (\(viewModel.distanceUnitDisplay))")
+
+                            HStack {
+                                Spacer()
+
+                                TextField(viewModel.distanceUnitDisplay, text: $viewModel.distance)
+                                    .keyboardType(.decimalPad)
+                                    .multilineTextAlignment(.trailing)
+                                    .frame(width: 100)
+                            }
+                        }
+                    }
+                }
+
+                Section {
+                    Button(action: {
+                        Task { await viewModel.logWorkout() }
+                    }) {
+                        HStack {
+                            Spacer()
+
+                            if viewModel.isLoading {
+                                ProgressView()
+                            } else {
+                                Text("Log Workout")
+                            }
+
+                            Spacer()
+                        }
+                    }
+                    .foregroundStyle(.orange)
+                    .disabled(viewModel.isLoading)
+                }
+
+                if let errorMessage = viewModel.errorMessage {
+                    Section {
+                        Text(errorMessage)
+                            .font(.caption)
+                            .foregroundColor(.red)
+                    }
+                }
+            }
+            .navigationTitle("Log New Workout")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel", role: .cancel) {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+
+    private var formatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimum = 0 // Ensure non-negative
+        return formatter
+    }
+
+}
+
+#Preview {
+    LogWorkoutView(healthKitManager: HealthKitManager())
 }
